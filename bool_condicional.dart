7cf4c19572cbd59320786b962036d24ee1382ed2
andres chavez <andreshtml5@gmail.com>
2019-07-09 23:28:32 -0500

    Create bool_condicional.dart

---
+++ bool_condicional.dart
@@ -0,0 +1,11 @@
+void main(){
+  bool activado = true;
+  print('activado');
+  
+  if( !activado /*activado == true*/ ){
+    print('El motor esta prendido');
+  }else
+  {
+    print('el motor esta apagado');
+  }
+}
